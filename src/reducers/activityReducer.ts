9aeef8b80ecbf1c7cd822e29dcfdd112a8546fa5
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-07-16 09:30:21 -0500

    añadir in formación al listado de actividades y cambiar bg de la actividad

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -1,30 +1,30 @@
-import { Activity } from "../types"
+import { Activity } from "../types";
 
-export type ActivityAcitions =
-    {type:'save-activity', payload:{newActivity:Activity}}
+export type ActivityAcitions = {
+  type: "save-activity";
+  payload: { newActivity: Activity };
+};
 
-export type ActivityState ={
-    activities : Activity[]
-}
+export type ActivityState = {
+  activities: Activity[];
+};
 
-
-export const initialState : ActivityState={
-    activities:[]
-
-}
+export const initialState: ActivityState = {
+  activities: [],
+};
 
 export const activityReducer = (
-    state:ActivityState =initialState,
-    action:ActivityAcitions
-)=>{
-    if(action.type==='save-activity'){
-        console.log(action.payload.newActivity)
+  state: ActivityState = initialState,
+  action: ActivityAcitions
+) => {
+  if (action.type === "save-activity") {
+    console.log(action.payload.newActivity);
 
-        return{
-            ...state,
-            activities: [...state.activities,action.payload.newActivity]
-        }
-    }
+    return {
+      ...state,
+      activities: [...state.activities, action.payload.newActivity],
+    };
+  }
 
-return state
-}
+  return state;
+};
